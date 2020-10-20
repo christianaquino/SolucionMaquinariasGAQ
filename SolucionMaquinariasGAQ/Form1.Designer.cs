27f5d4e8d821d00dc44a69aff1d4ad6ca2543471
CHRISTIAN JOSE AQUINO <christian.a77gmail.com>
2020-10-20 09:12:55 -0300

    Diseño form Login

--- SolucionMaquinariasGAQ/Form1.Designer.cs
+++ SolucionMaquinariasGAQ/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SolucionMaquinariasGAQ
 {
-    partial class Form1
+    partial class FormLogin
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,21 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(106, 128);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(117, 29);
+            this.buttonLogin.TabIndex = 3;
+            this.buttonLogin.Text = "Acceder";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelUsuario
+            // 
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.Location = new System.Drawing.Point(29, 30);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(47, 15);
+            this.labelUsuario.TabIndex = 1;
+            this.labelUsuario.Text = "Usuario";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(29, 80);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(57, 15);
+            this.labelPassword.TabIndex = 2;
+            this.labelPassword.Text = "Password";
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Location = new System.Drawing.Point(96, 27);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(142, 23);
+            this.textBoxUsuario.TabIndex = 1;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(96, 80);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(142, 23);
+            this.textBoxPassword.TabIndex = 2;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // FormLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 305);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(300, 214);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUsuario);
+            this.Controls.Add(this.buttonLogin);
+            this.Name = "FormLogin";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.TextBox textBoxPassword;
     }
 }
 
--- SolucionMaquinariasGAQ/Form1.cs
+++ SolucionMaquinariasGAQ/Form1.cs
@@ -10,14 +10,19 @@ using System.Windows.Forms;
 
 namespace SolucionMaquinariasGAQ
 {
-    public partial class Form1 : Form
+    public partial class FormLogin : Form
     {
-        public Form1()
+        public FormLogin()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
